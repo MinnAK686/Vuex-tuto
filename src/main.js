f49e121b44ae4a8504ec6faaaf1c3d84fe95203c
MinnAK686 <anastasioak327@gmail.com>
2022-12-15 16:07:38 +0630

    Vuex and Bootstrap-Vue setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
 
-createApp(App).mount('#app')
+// Bootstrap
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+const app = createApp(App);
+
+app.use(store);
+app.mount("#app");
---
+++ src/store/Todos/Todos.js
@@ -0,0 +1,6 @@
+export default {
+    state: {},
+    getter: {},
+    mutation: {},
+    actions: {},
+};
